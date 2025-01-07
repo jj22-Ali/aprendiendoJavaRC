70672f6e895f16df111a8ad8b92e38ec5265df6a
Juan Jose Figueroa <juanjosealifigueroa56@gmail.com>
2025-01-07 08:11:03 -0300

    Terminado la tarea de metodosString

--- 01 - introJs2025/js/02-tareas.js
+++ 01 - introJs2025/js/02-tareas.js
@@ -32,7 +32,15 @@ Ejemplo: 45+13: 58 o 45 es mayor que 13: true*/
 
 //? SOLUCION ----------------------------------------------------------------
 
- 
+let num1 = 10;
+let num2 = 20;
+
+console.log(`El resultado de la suma de ${num1} y ${num2} es: ${num1 + num2}`);
+console.log(`El resultado de la resta ${num1} y ${num2} es: ${num1 - num2}`);
+console.log(`El resultado de la multiplicación ${num1} y ${num2} es: ${num1 * num2}`);
+console.log(`El resultado de la division ${num1} y ${num2} es: ${num1/num2}`);
+console.log(`El ${num1} es mayor que ${num2}: ${num1 > num2}`);
+
 
 //* -------------------------------------------------------------------------
 
@@ -52,7 +60,19 @@ Ejemplo: 45+13: 58 o 45 es mayor que 13: true*/
 
 //? SOLUCION ----------------------------------------------------------------
 
- 
+let apellido = prompt('Ingrese su apellido: ');
+console.log(apellido);
+
+let apellidoEnmayuscula = apellido.toUpperCase();
+console.log(apellidoEnmayuscula); 
+
+let ApellidoSinEsp = apellidoEnmayuscula.trim();
+console.log(ApellidoSinEsp);
+
+console.log(`El Apellido tiene ${ApellidoSinEsp.length} caracteres`);
+
+console.log(`El ultimo caracter del apellido es: ${ApellidoSinEsp.at(0)}`);
+
 
 //* -------------------------------------------------------------------------
 
