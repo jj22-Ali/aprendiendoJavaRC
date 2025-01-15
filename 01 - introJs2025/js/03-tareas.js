4bafd078f607a8fef0bac6390354556364be094f
Juan Jose Figueroa <juanjosealifigueroa56@gmail.com>
2025-01-15 12:38:50 -0300

    terminada la teoria y hecho 1 ejercicios

---
+++ 01 - introJs2025/js/03-tareas.js
@@ -0,0 +1,32 @@
+//¡Tareas del dia!
+
+/*
+Tarea 1: 
+- Obtener un numero aleatorio entre 1 y 100
+- Calcular la raíz cuadrada y mostrar en consola
+- Elevarlo a la quinta potencia y mostrarlo en consola
+
+tarea 2: 
+- Crea una lista de numeros cualquiera y mostrar por consola el mayor y el menor
+
+tarea 3:
+- Dados los siguientes numeros, 
+46.84309
+82.32514
+53.44329
+- dejarlo con dos decimales y mostrarlo en consola
+*/
+
+//? Solucionea
+
+//* Tarea 1 
+let x = parseFloat((Math.random()*100).toFixed(2));
+console.log(`La raiz cuadrada de ${x} es: ${parseFloat(Math.sqrt(x).toFixed(2))}`);
+console.log(`La quinta potencia de ${x} es: ${parseFloat(Math.pow(x, 5).toFixed(2))}`);
+
+//* Tarea 2
+
+let lista = [32, 325, 1235, 745, 12];
+console.log(lista);
+console.log(`El numero mayor de la lista es: ${lista.Math}`);
+
