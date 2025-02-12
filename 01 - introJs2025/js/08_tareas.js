896184b95c2d8fb3d60402797d9aeaee1cc673fe
Juan Jose Figueroa <juanjosealifigueroa56@gmail.com>
2025-02-12 19:24:28 -0300

    terminado la secicon tarea 08

--- 01 - introJs2025/js/08_tareas.js
+++ 01 - introJs2025/js/08_tareas.js
@@ -1,6 +1,8 @@
 // Ejercicio 1: Crear una función con un callback personalizado
 // Deben crear una función llamada procesarDatos que reciba un array y un callback. El callback determinará cómo procesar los datos del array. Puede ser una suma, resta, multiplicacion o division como el ejemplo de clases, pero usando map o foreach
 
+//? asi lo hice yo
+/*
 let numeros = [23, 54, 91, 201, 73, 123];
 
 function sumar(numero){
@@ -9,13 +11,39 @@ function sumar(numero){
 
 function procesarDatos (numeros, sumar){
     
-    let resultado = numeros.map(sumar)
-
-    return console.log(resultado);
+    return numeros.map(sumar);
 }
 
-procesarDatos(numeros)
+console.log(procesarDatos(numeros, sumar)
+);
 
+
+*/
 
 // Ejercicio 2: Filtrar y encontrar nombres con callbacks
 // Deben filtrar un array de nombres para encontrar aquellos que comiencen con una letra específica. Luego, deben encontrar el primero que cumpla esa condición.
+
+
+let nombres = ['Pablo', 'Maria', 'Matias', 'Juan Jose', 'Cristian', 'Mauricio', 'Zaralegui', 'Maximiliano'];
+
+
+function encontrarNombres(nombres){
+    
+    let arrays =  nombres.filter((nombre) =>{
+        return  nombre.startsWith('M')
+    })
+    
+    return arrays;
+}
+
+function primerNombre(nombres, encontrarNombres){
+    let primero = nombres.find((encontrarNombres) =>{
+        return encontrarNombres.startsWith('M')
+    });
+
+    return primero;
+}
+
+console.log(nombres);
+console.log(encontrarNombres(nombres));
+console.log(primerNombre(nombres, encontrarNombres));
